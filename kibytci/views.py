018e2f38eedbd7689144575b9997421f4ea1fdab
Alex Burka <aburka@seas.upenn.edu>
2014-03-24 17:58:17 -0400

    use markdown in view

--- kibytci/views.py
+++ kibytci/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from kibytci.models import Gismu, Proposal
+from markdown import markdown
 
 def ro_gismu(request):
     gimste = Gismu.objects.all()
     for g in gimste:
         g.proposals = Proposal.objects.filter(gismu=g).order_by('klesi')
+        for p in g.proposals:
+            p.velcki = markdown(p.velcki)
     return render(request, 'ro_gismu.html',
                   {'gimste': gimste})
 
